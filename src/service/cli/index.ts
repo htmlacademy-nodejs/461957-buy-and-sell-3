3864f5db4f58247ffe2d27dc49f4bac5ed59e7c0
Artem Beresnev <artembert@gmail.com>
2020-01-26 12:49:32 +0300

    Service/CLI/Help add module

--- src/service/cli/index.ts
+++ src/service/cli/index.ts
@@ -1,8 +1,10 @@
 const version = require(`./version`);
 const generate = require(`./generate`);
+const help = require(`./help`);
 
 const Cli = {
   [generate.name]: generate,
+  [help.name]: help,
   [version.name]: version,
 };
 
