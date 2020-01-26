12408165e7b66312958895a3980b25a47dab5ca3
Artem Beresnev <artembert@gmail.com>
2020-01-26 13:38:35 +0300

    Service/CLI/Help replace "<" with "&lt;"

--- src/service/cli/help.ts
+++ src/service/cli/help.ts
@@ -6,12 +6,11 @@ const cliAction: CliAction = {
     console.info(`Программа запускает http-сервер и формирует файл с данными для API.
 
     Гайд:
-    server <command>
-
+    server &lt;command&gt;
     Команды:
     --version:            выводит номер версии
     --help:               печатает этот текст
-    --generate <count>    формирует файл mocks.json`)
+    --generate &lt;count&gt;    формирует файл mocks.json`)
   }
 };
 
