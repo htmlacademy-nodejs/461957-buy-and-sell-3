27f229375881feca03089f57d8f0764de013e359
Artem Beresnev <artembert@gmail.com>
2020-01-27 21:48:08 +0300

    Service/CLI console color messages: --help & --version

--- src/service/cli/help.ts
+++ src/service/cli/help.ts
@@ -1,16 +1,17 @@
 import {CliAction} from "../../types/cli-action";
+const chalk = require(`chalk`);
 
 const cliAction: CliAction = {
   name: `--help`,
   run() {
-    console.info(`Программа запускает http-сервер и формирует файл с данными для API.
+    console.info(chalk.gray(`Программа запускает http-сервер и формирует файл с данными для API.
 
     Гайд:
     server &lt;command&gt;
     Команды:
     --version:            выводит номер версии
     --help:               печатает этот текст
-    --generate &lt;count&gt;    формирует файл mocks.json`)
+    --generate &lt;count&gt;    формирует файл mocks.json`));
   }
 };
 
