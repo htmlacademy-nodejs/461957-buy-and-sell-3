bf95e8622d421f97a36ab5152d5dc978a3901b93
Artem Beresnev <artembert@gmail.com>
2020-01-27 23:28:24 +0300

    Service/CLI/Generate: read mock literals from external files

--- src/service/cli/generate.ts
+++ src/service/cli/generate.ts
@@ -3,37 +3,12 @@ import {Offer} from "../../types/offer";
 const fs = require(`fs`).promises;
 const {getRandomInt, shuffle} = require(`../../utils`);
 const chalk = require(`chalk`);
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
-
 const OfferType = {
   offer: `offer`,
   sale: `sale`,
@@ -47,12 +22,12 @@ const PictureRestrict = {
   max: 16,
 };
 
-function generateOffers(count: number): Offer[] {
+function generateOffers(count: number, categories: string[], sentences: string[], titles: string[]): Offer[] {
   return Array(count).fill({}).map(() => ({
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+    category: [categories[getRandomInt(0, categories.length - 1)]],
+    description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(SumRestrict.min, SumRestrict.max),
   }));
@@ -62,12 +37,30 @@ function getPictureFileName(amount: number): string {
   return amount > 10 ? `item${amount}.jpg` : `item0${amount}.jpg`;
 }
 
+async function readMockFile(filePath: string): Promise<string[]> {
+  try {
+    const rawContent: string = await fs.readFile(filePath, `UTF8`);
+    return rawContent
+      .replace(/(\r\n)/gm, `\n`)
+      .replace(/(\r)/gm, `\n`)
+      .split(`\n`)
+      .filter((value) => !!value.length);
+  } catch (e) {
+    console.error(chalk.red(`Filed to read ${filePath}`));
+    return [];
+  }
+}
+
 const cliAction: CliAction = {
   name: `--generate`,
   async run(args?) {
+    const categories = await readMockFile(FILE_CATEGORIES_PATH);
+    const sentences = await readMockFile(FILE_SENTENCES_PATH);
+    const titles = await readMockFile(FILE_TITLES_PATH);
     const [count] = args;
     const countOffers = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    await fs.writeFile(FILE_NAME, JSON.stringify(generateOffers(countOffers), undefined, 2));
+    const content = generateOffers(countOffers, categories, sentences, titles);
+    await fs.writeFile(FILE_NAME, JSON.stringify(content, undefined, 2));
     console.log(chalk.green(`${countOffers} offer(s) saved to ${FILE_NAME}`));
   }
 };
