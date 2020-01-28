af1372658448e0b5e21a9fe30c85dc58653b9759
Artem Beresnev <artembert@gmail.com>
2020-01-29 00:03:17 +0300

    Constants: add some http constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,10 +4,23 @@ const ExitCode = {
   error: 1,
   success: 0,
 };
+const DEFAULT_PORT = 3000;
+const HTTP_CODES = {
+  OK: 200,
+  notFound: 404,
+  internalServerError: 500,
+};
+const CONTENT_TYPE = {
+  text: `text/plain; charset=UTF-8`,
+  html: `text/html; charset=utf-8`
+};
 
 export = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  ExitCode
+  ExitCode,
+  DEFAULT_PORT,
+  HTTP_CODES,
+  CONTENT_TYPE,
 };
 
