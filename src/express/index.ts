b39e61622aa99428c2d8087336864f86e80418a0
Artem Beresnev <artembert@gmail.com>
2020-02-26 00:28:21 +0300

    Front: add routers: "search"

--- src/express/index.ts
+++ src/express/index.ts
@@ -3,6 +3,7 @@ const {SSR_PORT} = require(`../constants`);
 const registerRouter = require(`./routes/register`);
 const loginRouter = require(`./routes/login`);
 const myRouter = require(`./routes/my`);
+const searchRouter = require(`./routes/search`);
 
 const app = express();
 
@@ -10,4 +11,5 @@ app.get(`/`, (req, res) => res.send(`Hello, Express.js!`));
 app.use(`/register`, registerRouter);
 app.use(`/login`, loginRouter);
 app.use(`/my`, myRouter);
+app.use(`/search`, searchRouter);
 app.listen(SSR_PORT);
