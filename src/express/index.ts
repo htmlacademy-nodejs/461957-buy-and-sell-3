74b5e69cee8483b8319bae5a8853789b64b36b65
Artem Beresnev <artembert@gmail.com>
2020-02-26 00:17:15 +0300

    Front: add router "register"

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,8 +1,9 @@
 const express = require(`express`);
 const {SSR_PORT} = require(`../constants`);
+const registerRouter = require(`./routes/register`);
 
 const app = express();
 
 app.get(`/`, (req, res) => res.send(`Hello, Express.js!`));
-app.listen(SSR_PORT,
-  () => console.log(`Listen to port ${SSR_PORT}`));
+app.use(`/register`, registerRouter);
+app.listen(SSR_PORT);
