e86deb5454f3b9a4322273176f2faa5bb4c87e81
Artem Beresnev <aberesnev@intermedia.net>
2020-02-28 11:59:46 +0300

    Front: add routers: "offers"

--- src/express/routes/offers.ts
+++ src/express/routes/offers.ts
@@ -2,6 +2,20 @@ const {Router} = require(`express`);
 const offersRouter = new Router();
 
 offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
-offersRouter.get(`/category`, (req, res) => res.send(`/offers/category/:id ${req.params}`));
+offersRouter.get(`/edit/:id`, (req, res) => {
+  const offerId = Number.parseInt(req.params.id, 10);
+  return res.send(`/offers/edit/${offerId}`);
+});
+offersRouter.get(`/category/:id`, (req, res) => {
+  const categoryId = Number.parseInt(req.params.id, 10);
+  return res.send(`/offers/category/${categoryId}`);
+});
+offersRouter.get(`/:id`, (req, res, next) => {
+  const offerId = Number.parseInt(req.params.id, 10);
+  if (!isNaN(offerId)) {
+    return res.send(`/offers/${offerId}`);
+  }
+  next();
+});
 
 export = offersRouter;
