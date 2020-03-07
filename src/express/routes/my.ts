2e16dd333f1e0dc8327e58594dd877cddbc4795c
Artem Beresnev <artembert@gmail.com>
2020-03-07 13:29:19 +0300

    [module3-task2] Front: add template for "tickets-list"

--- src/express/routes/my.ts
+++ src/express/routes/my.ts
@@ -1,7 +1,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my`));
+myRouter.get(`/`, (req, res) => res.render(`pages/tickets-list`));
 myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
 
 export = myRouter;
