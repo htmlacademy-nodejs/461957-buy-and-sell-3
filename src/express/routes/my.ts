efaa99b9eac20e9bde9f3b828694bbc53461d9ef
Artem Beresnev <artembert@gmail.com>
2020-03-07 14:43:04 +0300

    [module3-task2] Front: comments: generate markup from data

--- src/express/routes/my.ts
+++ src/express/routes/my.ts
@@ -48,13 +48,69 @@ const ticketsList = [
     price: `1000`,
   },
 ];
+const commentsList = [
+  {
+    ticketHeader: {
+      title: `Ленд Ровер`,
+      price: `900 000`,
+      type: `ПРОДАМ`,
+    },
+    comments: [
+      {
+        author: `Александр Бурый`,
+        authorImageSrc: `img/avatar03.jpg`,
+        authorImageSrcSet: `img/avatar03@2x.jpg`,
+        content: `А что с прогоном автомобиля? Также вижу на фото зимнюю резину. А летняя идет ли впридачу?`,
+      },
+      {
+        author: `Анатолий Хакимов`,
+        authorImageSrc: `img/avatar04.jpg`,
+        authorImageSrcSet: `img/avatar04@2x.jpg`,
+        content: `Хочу прийти посмотреть на авто в среду. Мой телефон 89254455566. Готовы принять?`,
+      },
+      {
+        author: `Георгий Шпиц`,
+        authorImageSrc: `img/avatar02.jpg`,
+        authorImageSrcSet: `img/avatar02@2x.jpg`,
+        content: `Что это за рухлядь? Стыдно такое даже фотографировать, не то, что продавать.`,
+      },
+    ],
+  },
+  {
+    ticketHeader: {
+      title: `Ableton`,
+      price: `900 000`,
+      type: `ПРОДАМ`,
+    },
+    comments: [
+      {
+        author: `Александр Бурый`,
+        authorImageSrc: `img/avatar03.jpg`,
+        authorImageSrcSet: `img/avatar03@2x.jpg`,
+        content: `А что с прогоном автомобиля? Также вижу на фото зимнюю резину. А летняя идет ли впридачу?`,
+      },
+      {
+        author: `Анатолий Хакимов`,
+        authorImageSrc: `img/avatar04.jpg`,
+        authorImageSrcSet: `img/avatar04@2x.jpg`,
+        content: `Хочу прийти посмотреть на авто в среду. Мой телефон 89254455566. Готовы принять?`,
+      },
+      {
+        author: `Георгий Шпиц`,
+        authorImageSrc: `img/avatar02.jpg`,
+        authorImageSrcSet: `img/avatar02@2x.jpg`,
+        content: `Что это за рухлядь? Стыдно такое даже фотографировать, не то, что продавать.`,
+      },
+    ],
+  },
+];
 
 myRouter.get(`/`, (req, res) => {
   const pageContent = {ticketsList, isLogged: true};
   res.render(`pages/tickets-list`, pageContent);
 });
 myRouter.get(`/comments`, (req, res) => {
-  const pageContent = {isLogged: true};
+  const pageContent = {commentsList, isLogged: true};
   res.render(`pages/comments`, pageContent);
 });
 
