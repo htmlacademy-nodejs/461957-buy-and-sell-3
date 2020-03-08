a410ec45adb19748b7828e7c98a14574cd25ebeb
Artem Beresnev <artembert@gmail.com>
2020-03-08 22:14:35 +0300

    [module3-task2] Front: Main Page: add tickets

--- src/express/routes/main-page.ts
+++ src/express/routes/main-page.ts
@@ -4,8 +4,9 @@ const mainPageRouter = new Router();
 
 mainPageRouter.get(`/`, (req, res) => {
   const pageContent = {
-    categoriesList: getCategoriesList(16),
-    tickets: getTickets(5),
+    categoriesList: getCategoriesList(6),
+    newTickets: getTickets(8),
+    popularTickets: getTickets(4),
   };
   res.render(`pages/main`, pageContent);
 });
