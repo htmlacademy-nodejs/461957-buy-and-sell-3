a1e872debbb26feb61c504e13c7908a9801bbffd
Artem Beresnev <artembert@gmail.com>
2020-03-08 10:13:00 +0300

    [module3-task2] Front: Search: add suggestions

--- src/express/routes/search.ts
+++ src/express/routes/search.ts
@@ -24,10 +24,94 @@ const ticketsList = [
   },
 ];
 
+const suggestionsList = [
+  {
+    title: `Ableton`,
+    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
+    imageSrcSet: `img/item06@2x.jpg 2x`,
+    label: `ПРОДАМ`,
+    color: `color06`,
+    categoriesList: [`ЭЛЕКТРОНИКА`],
+    price: `88 000`,
+    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
+  },
+  {
+    title: `Мое старое кресло`,
+    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
+    imageSrcSet: `img/item10@2x.jpg 2x`,
+    label: `ПРОДАМ`,
+    color: `color10`,
+    categoriesList: [`Дом`],
+    price: `4000`,
+    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
+  },
+  {
+    title: `Ableton`,
+    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
+    imageSrcSet: `img/item06@2x.jpg 2x`,
+    label: `ПРОДАМ`,
+    color: `color06`,
+    categoriesList: [`ЭЛЕКТРОНИКА`],
+    price: `88 000`,
+    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
+  },
+  {
+    title: `Мое старое кресло`,
+    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
+    imageSrcSet: `img/item10@2x.jpg 2x`,
+    label: `ПРОДАМ`,
+    color: `color10`,
+    categoriesList: [`Дом`],
+    price: `4000`,
+    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
+  },
+  {
+    title: `Ableton`,
+    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
+    imageSrcSet: `img/item06@2x.jpg 2x`,
+    label: `ПРОДАМ`,
+    color: `color06`,
+    categoriesList: [`ЭЛЕКТРОНИКА`],
+    price: `88 000`,
+    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
+  },
+  {
+    title: `Мое старое кресло`,
+    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
+    imageSrcSet: `img/item10@2x.jpg 2x`,
+    label: `ПРОДАМ`,
+    color: `color10`,
+    categoriesList: [`Дом`],
+    price: `4000`,
+    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
+  },
+  {
+    title: `Ableton`,
+    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
+    imageSrcSet: `img/item06@2x.jpg 2x`,
+    label: `ПРОДАМ`,
+    color: `color06`,
+    categoriesList: [`ЭЛЕКТРОНИКА`],
+    price: `88 000`,
+    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
+  },
+  {
+    title: `Мое старое кресло`,
+    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
+    imageSrcSet: `img/item10@2x.jpg 2x`,
+    label: `ПРОДАМ`,
+    color: `color10`,
+    categoriesList: [`Дом`],
+    price: `4000`,
+    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
+  },
+];
+
 searchRouter.get(`/`, (req, res) => {
   const pageContent = {
     resultCount: 2,
     ticketsList,
+    suggestionsList,
   };
   res.render(`pages/search`, pageContent);
 });
