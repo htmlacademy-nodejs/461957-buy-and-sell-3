56336d545e71c4e2dae0a819e1f44ee39d86ddeb
Artem Beresnev <artembert@gmail.com>
2020-03-08 22:49:41 +0300

    [module3-task2] Front: replace inline mocks with data-providers functions

--- src/express/routes/search.ts
+++ src/express/routes/search.ts
@@ -1,117 +1,12 @@
 const {Router} = require(`express`);
+const {getTickets} = require(`../services/data-provider`);
 const searchRouter = new Router();
-
-const ticketsList = [
-  {
-    title: `Ableton`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
-    imageSrcSet: `img/item06@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color06`,
-    categoriesList: [`ЭЛЕКТРОНИКА`],
-    price: `88 000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-  {
-    title: `Мое старое кресло`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
-    imageSrcSet: `img/item10@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color10`,
-    categoriesList: [`Дом`],
-    price: `4000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-];
-
-const suggestionsList = [
-  {
-    title: `Ableton`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
-    imageSrcSet: `img/item06@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color06`,
-    categoriesList: [`ЭЛЕКТРОНИКА`],
-    price: `88 000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-  {
-    title: `Мое старое кресло`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
-    imageSrcSet: `img/item10@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color10`,
-    categoriesList: [`Дом`],
-    price: `4000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-  {
-    title: `Ableton`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
-    imageSrcSet: `img/item06@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color06`,
-    categoriesList: [`ЭЛЕКТРОНИКА`],
-    price: `88 000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-  {
-    title: `Мое старое кресло`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
-    imageSrcSet: `img/item10@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color10`,
-    categoriesList: [`Дом`],
-    price: `4000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-  {
-    title: `Ableton`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
-    imageSrcSet: `img/item06@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color06`,
-    categoriesList: [`ЭЛЕКТРОНИКА`],
-    price: `88 000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-  {
-    title: `Мое старое кресло`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
-    imageSrcSet: `img/item10@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color10`,
-    categoriesList: [`Дом`],
-    price: `4000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-  {
-    title: `Ableton`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item06.jpg`,
-    imageSrcSet: `img/item06@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color06`,
-    categoriesList: [`ЭЛЕКТРОНИКА`],
-    price: `88 000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-  {
-    title: `Мое старое кресло`,
-    imageSrc: `http://localhost:63342/buy-and-sell-3/markup/img/item10.jpg`,
-    imageSrcSet: `img/item10@2x.jpg 2x`,
-    label: `ПРОДАМ`,
-    color: `color10`,
-    categoriesList: [`Дом`],
-    price: `4000`,
-    description: `Куплю монстеру зеленую в хорошем зеленом состоянии, буду поливать...`,
-  },
-];
 
 searchRouter.get(`/`, (req, res) => {
   const pageContent = {
     resultCount: 2,
-    ticketsList,
-    suggestionsList,
+    ticketsList: getTickets(2),
+    suggestionsList: getTickets(8),
   };
   res.render(`pages/search`, pageContent);
 });
