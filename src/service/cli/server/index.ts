42dc50e2cd0d7188a93cedf00d9f285b814d705c
Artem Beresnev <artembert@gmail.com>
2020-03-15 10:23:17 +0300

    [module3-task3] Service: return JSON offers

--- src/service/cli/server/index.ts
+++ src/service/cli/server/index.ts
@@ -1,20 +1,18 @@
 import {Express} from "express";
 
-
 const express = require(`express`);
 const offersRouter = require(`./routes/offers`);
 const chalk = require(`chalk`);
-const { DEFAULT_PORT } = require(`../../../constants`);
+const {DEFAULT_PORT} = require(`../../../constants`);
 
-function runServer(args) {
+function runServer(args?) {
   const [customPort] = args;
   const port = parseInt(customPort, 10) || DEFAULT_PORT;
   const app: Express = express();
 
   app.use(`/offers`, offersRouter);
 
-  app.listen(port);
-  console.info(chalk.green(`Listen on port ${port}`));
+  app.listen(port, () => console.info(chalk.green(`Listen on port ${port}`)));
 }
 
 export = runServer;
--- src/service/cli/server/routes/offers.ts
+++ src/service/cli/server/routes/offers.ts
@@ -1,6 +1,21 @@
+import {Offer} from "../../../../types/offer";
+
+const {MOCK_FILE_PATH} = require(`../../../../constants`);
+const fs = require(`fs`).promises;
+
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/`, (req, res) => res.send({response: `response`}));
+offersRouter.get(`/`, async (req, res) => {
+  let offersContent;
+  try {
+    const rawOffers = await fs.readFile(MOCK_FILE_PATH, `utf8`);
+    offersContent = JSON.parse(rawOffers) as Offer[];
+  } catch (e) {
+    offersContent = [];
+  }
+
+  res.json(offersContent);
+});
 
 export = offersRouter;
