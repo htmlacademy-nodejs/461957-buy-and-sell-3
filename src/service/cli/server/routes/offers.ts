67d8eeea4cff518d8a5b44b4164e11eda0ad9e6c
Artem Beresnev <artembert@gmail.com>
2020-03-22 23:06:56 +0300

    [module4-task1] Service:Server: Offers: Add types

--- src/service/cli/server/routes/offers.ts
+++ src/service/cli/server/routes/offers.ts
@@ -1,12 +1,13 @@
 import {Offer} from "../../../../types/offer";
+import * as TExpress from "express";
 
 const {MOCK_FILE_PATH} = require(`../../../../constants`);
 const fs = require(`fs`).promises;
-
 const {Router} = require(`express`);
-const offersRouter = new Router();
 
-offersRouter.get(`/`, async (req, res) => {
+const offersRouter: TExpress.Router = new Router();
+
+offersRouter.get(`/`, async (req: TExpress.Request, res: TExpress.Response) => {
   let offersContent;
   try {
     const rawOffers = await fs.readFile(MOCK_FILE_PATH, `utf8`);
