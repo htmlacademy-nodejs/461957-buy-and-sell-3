1c0954255f26576d069d357cc93feec906198039
Artem Beresnev <artembert@gmail.com>
2020-03-23 00:00:37 +0300

    [module4-task1] Service: Server: move business logic from OffersRouter to offersService

--- src/service/cli/server/routes/offers.ts
+++ src/service/cli/server/routes/offers.ts
@@ -1,22 +1,13 @@
-import {Offer} from "../../../../types/offer";
 import * as TExpress from "express";
+import OffersService from "../services/offers-service";
 
-const {MOCK_FILE_PATH} = require(`../../../../constants`);
-const fs = require(`fs`).promises;
 const {Router} = require(`express`);
 
 const offersRouter: TExpress.Router = new Router();
+const offersService: OffersService = new OffersService();
 
 offersRouter.get(`/`, async (req: TExpress.Request, res: TExpress.Response) => {
-  let offersContent;
-  try {
-    const rawOffers = await fs.readFile(MOCK_FILE_PATH, `utf8`);
-    offersContent = JSON.parse(rawOffers) as Offer[];
-  } catch (e) {
-    offersContent = [];
-  }
-
-  res.json(offersContent);
+  res.json(await offersService.getOffers());
 });
 
 export = offersRouter;
