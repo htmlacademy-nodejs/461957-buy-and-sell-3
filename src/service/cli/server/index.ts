352ae2b94b0555b9f618d66e7a78a8b6499b4305
Artem Beresnev <artembert@gmail.com>
2020-03-24 23:43:50 +0300

    [module4-task1] Service: Server: install & use "body-parser"

--- src/service/cli/server/index.ts
+++ src/service/cli/server/index.ts
@@ -1,6 +1,7 @@
 import {Express} from "express";
 
 const express = require(`express`);
+const bodyParser = require(`body-parser`);
 const apiRouter = require(`./routes/api`);
 const chalk = require(`chalk`);
 const {DEFAULT_PORT} = require(`../../../constants`);
@@ -9,7 +10,7 @@ function runServer(args?) {
   const [customPort] = args;
   const port = parseInt(customPort, 10) || DEFAULT_PORT;
   const app: Express = express();
-
+  app.use(bodyParser.json());
   app.use(`/api`, apiRouter);
 
   app.listen(port, () => console.info(chalk.green(`Listen on port ${port}`)));
