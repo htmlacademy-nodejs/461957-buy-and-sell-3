3cbc0a3b09d23742d128bebbc39f4df0311200a1
Artem Beresnev <artembert@gmail.com>
2020-04-18 15:16:08 +0300

    [module4-task2] offers.e2e: wrap tests into describe

--- src/service/cli/server/routes/offers.e2e.test.ts
+++ src/service/cli/server/routes/offers.e2e.test.ts
@@ -24,78 +24,80 @@ const invalidNewOffer = {
   type: ``,
 };
 
-test(`When get offers list status code should be 200`, async () => {
-  const res = await request(app).get(`/api/offers/`);
-  expect(res.status).toBe(200);
-});
+describe(`Offers API end-points`, () => {
+  test(`When get offers list status code should be 200`, async () => {
+    const res = await request(app).get(`/api/offers/`);
+    expect(res.status).toBe(200);
+  });
 
-test(`Should return array when request offers`, async () => {
-  const res = await request(app).get(`/api/offers/`);
-  expect(Array.isArray(res.body)).toBe(true);
-});
+  test(`Should return array when request offers`, async () => {
+    const res = await request(app).get(`/api/offers/`);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
 
-test(`Should return code 200 when request offer with test id ${validOfferId}`, async () => {
-  const res = await request(app).get(`/api/offers/${validOfferId}`);
-  expect(res.status).toBe(200);
-});
+  test(`Should return code 200 when request offer with test id ${validOfferId}`, async () => {
+    const res = await request(app).get(`/api/offers/${validOfferId}`);
+    expect(res.status).toBe(200);
+  });
 
-test(`Should return code 404 when request offer with invalid id '${invalidOfferId}'`, async () => {
-  const res = await request(app).get(`/api/offers/${invalidOfferId}`);
-  expect(res.status).toBe(404);
-});
+  test(`Should return code 404 when request offer with invalid id '${invalidOfferId}'`, async () => {
+    const res = await request(app).get(`/api/offers/${invalidOfferId}`);
+    expect(res.status).toBe(404);
+  });
 
-test(`Should return offer with defined fields`, async () => {
-  const res = await request(app).get(`/api/offers/${validOfferId}`);
-  const responseKeys = Object.keys(res.body) as string[];
-  expect(responseKeys).toContain(`id`);
-  expect(responseKeys).toContain(`category`);
-  expect(responseKeys).toContain(`description`);
-  expect(responseKeys).toContain(`picture`);
-  expect(responseKeys).toContain(`title`);
-  expect(responseKeys).toContain(`type`);
-  expect(responseKeys).toContain(`sum`);
-  expect(responseKeys).toContain(`comments`);
-});
+  test(`Should return offer with defined fields`, async () => {
+    const res = await request(app).get(`/api/offers/${validOfferId}`);
+    const responseKeys = Object.keys(res.body) as string[];
+    expect(responseKeys).toContain(`id`);
+    expect(responseKeys).toContain(`category`);
+    expect(responseKeys).toContain(`description`);
+    expect(responseKeys).toContain(`picture`);
+    expect(responseKeys).toContain(`title`);
+    expect(responseKeys).toContain(`type`);
+    expect(responseKeys).toContain(`sum`);
+    expect(responseKeys).toContain(`comments`);
+  });
 
-test(`Should return code 200 when send valid offer`, async () => {
-  const res = await request(app).post(`/api/offers/`).send(validNewOffer);
-  expect(res.status).toBe(200);
-});
+  test(`Should return code 200 when send valid offer`, async () => {
+    const res = await request(app).post(`/api/offers/`).send(validNewOffer);
+    expect(res.status).toBe(200);
+  });
 
-test(`Should return offer with id when request to add offer`, async () => {
-  const res = await request(app).post(`/api/offers/`).send(validNewOffer);
-  expect(res.body.hasOwnProperty(`id`)).toBe(true);
-});
+  test(`Should return offer with id when request to add offer`, async () => {
+    const res = await request(app).post(`/api/offers/`).send(validNewOffer);
+    expect(res.body.hasOwnProperty(`id`)).toBe(true);
+  });
 
-test(`Should return code 400 when send invalid offer`, async () => {
-  const res = await request(app).post(`/api/offers/`).send(invalidNewOffer);
-  expect(res.status).toBe(400);
-});
+  test(`Should return code 400 when send invalid offer`, async () => {
+    const res = await request(app).post(`/api/offers/`).send(invalidNewOffer);
+    expect(res.status).toBe(400);
+  });
 
-test(`Should return validation error when send invalid offer`, async () => {
-  const res = await request(app).post(`/api/offers/`).send(invalidNewOffer);
-  const validationKeys = Object.keys(res.body) as string[];
-  expect(validationKeys).toContain(`picture`);
-  expect(validationKeys).toContain(`sum`);
-  expect(validationKeys).toContain(`category`);
-  expect(validationKeys).toContain(`description`);
-  expect(validationKeys).toContain(`title`);
-  expect(validationKeys).toContain(`type`);
-});
+  test(`Should return validation error when send invalid offer`, async () => {
+    const res = await request(app).post(`/api/offers/`).send(invalidNewOffer);
+    const validationKeys = Object.keys(res.body) as string[];
+    expect(validationKeys).toContain(`picture`);
+    expect(validationKeys).toContain(`sum`);
+    expect(validationKeys).toContain(`category`);
+    expect(validationKeys).toContain(`description`);
+    expect(validationKeys).toContain(`title`);
+    expect(validationKeys).toContain(`type`);
+  });
 
-test(`Should return code 200 when update offer`, async () => {
-  const res = await request(app)
-    .put(`/api/offers/`)
-    .send({...validNewOffer, id: validOfferId});
-  expect(res.status).toBe(200);
-});
+  test(`Should return code 200 when update offer`, async () => {
+    const res = await request(app)
+      .put(`/api/offers/`)
+      .send({...validNewOffer, id: validOfferId});
+    expect(res.status).toBe(200);
+  });
 
-test(`Should return code 200 when delete offer`, async () => {
-  const res = await request(app).delete(`/api/offers/${validOfferId}`);
-  expect(res.status).toBe(200);
-});
+  test(`Should return code 200 when delete offer`, async () => {
+    const res = await request(app).delete(`/api/offers/${validOfferId}`);
+    expect(res.status).toBe(200);
+  });
 
-test(`Should return code 404 when delete nonexistent offer`, async () => {
-  const res = await request(app).delete(`/api/offers/${invalidOfferId}`);
-  expect(res.status).toBe(404);
+  test(`Should return code 404 when delete nonexistent offer`, async () => {
+    const res = await request(app).delete(`/api/offers/${invalidOfferId}`);
+    expect(res.status).toBe(404);
+  });
 });
