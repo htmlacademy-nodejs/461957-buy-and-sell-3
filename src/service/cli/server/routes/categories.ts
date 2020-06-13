68e0fdc4f45c765ed00950936a1e2795482bf52a
Artem Beresnev <artembert@gmail.com>
2020-06-13 22:57:48 +0300

    [module4-task2-pino] Log status codes of responses: categories.controller and search.controller

--- src/service/cli/server/routes/categories.ts
+++ src/service/cli/server/routes/categories.ts
@@ -1,10 +1,20 @@
 import {Router, Request, Response} from "express";
 import offersService from "../services/offers.service";
+import {getLogger} from "../logger";
+import {HttpCodes} from "../../../../shared/http-codes";
 
 const categoriesRouter: Router = Router();
+const logger = getLogger();
 
 categoriesRouter.get(`/`, async (req: Request, res: Response) => {
-  res.json(await offersService.getCategories());
+  try {
+    res.json(await offersService.getCategories());
+    logger.info(`End request with status code ${res.statusCode}`);
+  } catch (e) {
+    logger.error(e);
+    res.status(HttpCodes.INTERNAL_SERVER_ERROR).send();
+    logger.info(`End request with status code ${res.statusCode}`);
+  }
 });
 
 export default categoriesRouter;
--- src/service/cli/server/routes/search.ts
+++ src/service/cli/server/routes/search.ts
@@ -1,11 +1,20 @@
 import {Request, Response, Router} from "express";
 import offersService from "../services/offers.service";
+import {HttpCodes} from "../../../../shared/http-codes";
+import {getLogger} from "../logger";
 
 const searchRouter: Router = Router();
+const logger = getLogger();
 
 searchRouter.get(``, async (req: Request, res: Response) => {
-  const query = req.query.query;
-  res.json(await offersService.searchByOfferTitle(query));
+  try {
+    res.json(await offersService.searchByOfferTitle(req.query.query));
+    logger.info(`End request with status code ${res.statusCode}`);
+  } catch (e) {
+    logger.error(e);
+    res.status(HttpCodes.INTERNAL_SERVER_ERROR).send();
+    logger.info(`End request with status code ${res.statusCode}`);
+  }
 });
 
 export default searchRouter;
